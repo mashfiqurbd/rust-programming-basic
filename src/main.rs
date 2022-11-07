ffe5e506ffa84d32916997f8669d4befa860112d
mashfiqurbd <mashfiqur2020@gmail.com>
2022-11-07 07:23:45 +0600

    Rust Programming Language! Printing hello world

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,4 @@
 fn main() {
-    // this is a rust programming language comments
-    /*this is a double
-    line comment in
-    rust programming
-    language*/
-    fn main() {
-        println!("Hello, world!");
-    
-        another_function();
-    }
-    
-    fn another_function() {
-        println!("Another function.");
-    }
-    
+    // print hello world in rust :)
+    println!("Hello, world!");
 }
