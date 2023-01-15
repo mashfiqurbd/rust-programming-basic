a92e26aee1fe04830144ad37a75a44cf5857a05b
mashfiqurbd <mashfiqur2020@gmail.com>
2023-01-15 12:34:01 +0600

    processing user input guess

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,14 @@
+use std::io;
+
 fn main() {
-    // print hello world in rust :)
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+    
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {}", guess);
 }
